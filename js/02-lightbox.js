58c39f88ad7280821d9128cbcff31785cb5e93d4
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-04-24 22:03:38 +0300

    решает задачу №2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+const gallery = document.querySelector('.gallery');
+const picturesMarkup =  createPicturesMarkup(galleryItems);
+
+
+gallery.insertAdjacentHTML('beforeend', picturesMarkup);
+
+
+
+function createPicturesMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+
+
+  
+
+
+
